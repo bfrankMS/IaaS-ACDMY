7f766ac13a97c78be8debd0a64441d4f3788ed82
Bernhard Frank <bfrank@microsoft.com>
2019-03-26 16:26:22 +0100

    Automation

---
+++ Labs/Übung xx/1 - ARM/4 - DeployARMTemplate.ps1
@@ -0,0 +1,57 @@
+﻿<#
+    Bootcamp: Intelligent Cloud Bootcamp Azure IaaS Cloud Expert
+
+    ***************************************
+
+        ...................
+
+    ***************************************
+    This script will 
+
+    see: https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-quickstart-create-templates-use-visual-studio-code?tabs=CLI
+
+    by: bfrank
+    version: 1.0.0.0
+#>
+
+#region variables
+    $ARMRG = "ACDMY-ARM"
+    $Location = "North Europe"
+#endregion
+
+<# Login to Azure
+#Note: you may comment this out in case you already logged in with PowerShell
+Login-AzAccount -Environment AzureCloud
+
+#select the right subscription
+Get-AzSubscription | Out-GridView -Title "Welche Subscription soll verwendet werden?" -PassThru | Select-AzSubscription
+#>
+
+New-AZResourceGroup -Name $ARMRG -Location $Location
+
+#copy URI of sample template to clipboard
+"https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-storage-account-create/azuredeploy.json" | clip
+
+<#Do:
+1) VSCODE -> File -> Open File -> Press 'STRG-V'
+Save file as "sa.json" in Folder
+
+2) Open 'sa.json' with VSCode. 
+ollapse view with 'Hold STRG press K then 0'
+
+3) expand -> resources , parameters, outputs...
+
+4) add below to output section 
+"storageUri": {
+  "type": "string",
+  "value": "[reference(variables('storageAccountName')).primaryEndpoints.blob]"
+}
+#>
+
+#Deploy template to Azure
+New-AZResourceGroupDeployment -TemplateFile ".\sa.json" -ResourceGroupName $ARMRG 
+
+#can you see the StorageURI in Terminal output?
+
+#cleanup
+#Remove-AZResourceGroup -Name $ARMRG -Force -AsJob
---
+++ Labs/Übung xx/4 - What To Do With The VM/RunBook-ListVMsStateAndSize.ps1
@@ -0,0 +1,49 @@
+﻿<#
+    .DESCRIPTION
+        An example runbook which gets all the ARM resources using the Run As Account (Service Principal)
+
+    .NOTES
+        AUTHOR: Azure Automation Team
+        LASTEDIT: Mar 14, 2016
+#>
+
+$connectionName = "AzureRunAsConnection"
+
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+$rmvms=Get-AzureRmVM -Status
+# Add info about VM's from the Resource Manager to the array
+foreach ($vm in $rmvms)
+{    
+     # Add values to the array:
+    $vmarray += New-Object PSObject -Property @{
+            RG=$vm.ResourceGroupName; 
+            Name=$vm.Name; 
+            PowerState=$vm.PowerState ; #(get-culture).TextInfo.ToTitleCase(($vm.statuses)[1].code.split("/")[1]); 
+            Size=$vm.HardwareProfile.VmSize
+            'UTCTimestamp' = (Get-Date).ToUniversalTime().ToString("yyyy-MM-dd HH:mm")
+    }
+}
+
+$vmarray | Select-Object 'UTCTimestamp',Name,RG,PowerState,Size | ft
